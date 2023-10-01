e7641007b7f2bc60c62b66ef5ae6f1c800ddd7ce
AdrianSilvaNascimento <adrianfsf10@gmail.com>
2023-10-01 15:21:29 -0300

    Add the navigation and header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component'
+import { InfoItemComponent } from './components/info-item/info-item.component'
+import { RegisterItemComponent } from './components/register-item/register-item.component'
+
+const routes: Routes = [
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+  { path: 'index', component: HomeComponent },
+  { path: 'info-item/:id', component: InfoItemComponent },
+  { path: 'register-item', component: RegisterItemComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { AppRoutingModule } from './app-routing.module'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatSidenavModule } from '@angular/material/sidenav'
+
+import { AppComponent } from './app.component'
+import { HomeComponent } from './components/home/home.component'
+import { RegisterItemComponent } from './components/register-item/register-item.component'
+import { InfoItemComponent } from './components/info-item/info-item.component'
+import { HeaderComponent } from './components/header/header.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterItemComponent,
+    InfoItemComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatSidenavModule,
+    FontAwesomeModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+
+export class AppModule {}
