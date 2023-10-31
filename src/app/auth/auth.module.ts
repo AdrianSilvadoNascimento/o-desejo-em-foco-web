08fce132beccbe0ace8b990a76308587cd39e5d5
AdrianSilvaNascimento <adrianfsf10@gmail.com>
2023-10-31 15:01:43 -0300

    add some performance

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
+import { InjectionToken, NgModule } from '@angular/core'
+import { CommonModule, ImageLoaderConfig } from '@angular/common'
+import { NgOptimizedImage } from '@angular/common'
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { RouterModule } from '@angular/router'
@@ -11,6 +12,8 @@ import { AngularMaterialModule } from '../angular-material.module'
 import { LoginComponent } from './login/login.component'
 import { RegisterComponent } from './register/register.component'
 
+const IMAGE_CONFIG: InjectionToken<ImageLoaderConfig> = new InjectionToken<ImageLoaderConfig>('')
+
 @NgModule({
   declarations: [
     LoginComponent,
@@ -20,12 +23,21 @@ import { RegisterComponent } from './register/register.component'
     CommonModule,
     AngularMaterialModule,
     FontAwesomeModule,
+    NgOptimizedImage,
     RouterModule.forChild([
       { path: 'user-login', component: LoginComponent },
       { path: 'user-register', component: RegisterComponent },
     ]),
     FormsModule,
     ReactiveFormsModule,
+  ],
+  providers: [
+    {
+      provide: IMAGE_CONFIG,
+      useValue: {
+        breakpoints: [16, 48, 96, 128, 384, 640, 750, 828, 1080, 1200, 1920]
+      }
+    },
   ],
 })
 export class AuthModule {}
